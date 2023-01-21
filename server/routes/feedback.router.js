d533e274e9203d3cefbd8dc938ad32d50792af7e
Shong Lee <Shonglee17@gmail.com>
2023-01-21 07:38:48 -0600

    established post route on server side:

--- server/routes/feedback.router.js
+++ server/routes/feedback.router.js
@@ -2,14 +2,42 @@ const express = require('express');
 const router = express.Router();
 const pool = require('../modules/pool');
 
+
+//gets all information from database
 router.get('/', (req, res) => {
-    console.log('GET /api/pizza');
-    pool.query('SELECT * from "feedback";').then((result) => {
+    
+    pool.query('SELECT * from "feedback";')
+    .then((result) => {
         res.send(result.rows);
     }).catch((error) => {
-        console.log('Error GET /api/pizza', error)
+        console.log('Error GET /feedback', error)
         res.sendStatus(500);
     });
 })
+
+//sends information from client side to serverside database
+router.post('/', (req, res) => {
+    let feeling = req.body.feeling
+    let understanding = req.body.understanding
+    let support = req.body.support
+    let comments = req.body.comments
+    let flagged = req.body.flagged
+    let date = req.body.date
+
+    let sqlValues = [feeling, understanding, support, comments, flagged, date]
+
+    let queryText = 
+    'INSERT INTO "feedback" ("feeling", "understanding", "support", "comments", "flagged", "date")VALUES ($1, $2, $3, $4, $5, $6);'
+    
+    pool.query(queryText, sqlValues)
+    .then((result) => {
+        res.sendStatus(200);
+    }).catch((err) => {
+        console.log(err);
+        res.sendStatus(500);
+    });
+});
+
+
 
 module.exports = router;
