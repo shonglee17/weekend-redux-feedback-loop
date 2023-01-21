32cc85ecd09f5bb7d5980c14b4e39b5f075f4675
Shong Lee <Shonglee17@gmail.com>
2023-01-21 07:21:14 -0600

    established get route on server side

--- server/server.js
+++ server/server.js
@@ -2,14 +2,15 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 5000;
-
+const feedbackRouter = require('./routes/feedback.router.js');
 /** ---------- MIDDLEWARE ---------- **/
 app.use(bodyParser.json()); 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('build'));
 
 /** ---------- EXPRESS ROUTES ---------- **/
-
+//create routes on server side to talk to database
+app.use('/prime_feedback', feedbackRouter);
 
 /** ---------- START SERVER ---------- **/
 app.listen(PORT, () => {
